08f337688fd3dea566b677b6d7ec37bdebbc7801
Bilal Hameed <77564057+BilalHameed22@users.noreply.github.com>
2025-07-09 20:32:09 +0500

    Macros added for handler file.
    These include,
    1) try_handler
    2) lock
     3) get_http_param

--- src/handler.rs
+++ src/handler.rs
@@ -10,4 +10,36 @@ use router::Router;
 use models::Post;
 use std::error::Error;
 
+macro_rules! try_handler {
+    ($e:expr) => {
+        match $e {
+            Ok(x) => x,
+            Err(e) => return Ok(Response::with(status::InternalServerError, e.description())
+        }
 
+    };
+    ($e: expr, $error:expr) =>{
+        match $e {
+            Ok(x) => x,
+            Err(e) => return Ok(Response::with(($error, e.description())))
+        }
+    }
+}
+
+macro_rules! lock {
+    ($e:expr) => {e.lock().unwrap()}
+}
+macro_rules! get_http_param {
+    ($r:expr, $e:expr) => {
+        match $r.extentions.get::<Router()>{
+            Some(router) => {
+                match router.find($e){
+                    Some(v) => v,
+                    None => return Ok(Response::with(status::BadRequest))
+                }
+
+            },
+            None => return Ok(Response::with(status::InternalServerError))
+        }
+    } 
+}
